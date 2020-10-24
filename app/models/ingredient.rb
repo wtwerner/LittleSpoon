61eab21a5089b07bb1080c474ddd6e9abf2a6bd0
wtwerner <wtwerner@gmail.com>
2020-10-24 16:40:56 -0500

    Remove has_many_and_belongs_to associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :recipes
+  has_many :ingredients_recipes
+  has_many :recipes, through: :ingredients_recipes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
-  has_and_belongs_to_many :ingredients
-  has_and_belongs_to_many :categories
+  has_many :ingredients_recipes
+  has_many :categories_recipes
+  has_many :ingredients, through: :ingredients_recipes
+  has_many :categories, through: :categories_recipes
   has_many :steps
 end
