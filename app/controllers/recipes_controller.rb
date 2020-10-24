f1b6bd8a03dcbf01af3fd5ea060d1ca6a7503eb1
wtwerner <wtwerner@gmail.com>
2020-10-23 22:05:24 -0500

    Create recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,69 @@
 class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def index
+        @recipe = Recipe.all
+    end
+
+    def show
+        
+    end
+
+    def edit
+
+    end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+        
+        respond_to do |format|
+            if @recipe.save
+                format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
+                format.json { render :show, status: :created, location: @recipe }
+            else
+                format.html { render :new }
+                format.json { render json: @recipe.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @recipe.update(recipe_params)
+                format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.'}
+                format.json { render :show, status: :ok, location: @recipe }
+            else
+                format.html { render :edit }
+                format.json { render json: @recipe.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        @recipe.destroy
+        respond_to do |format|
+            format.html { redirect_to recipes_url, notice: 'Recipe was successfully deleted.' }
+            format.json { head :no_content }
+        end
+    end
+
+    private
+
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def meal_params
+        params.require(:recipe).permit(
+            :user_id,
+            :name,
+            :category_id,
+            :favorite,
+            ingredients_attributes: [:id, :name, :recipe_id]
+        )
+    end
 end
