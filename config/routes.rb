badc27d197c313fd682e46b5e3c57857ae17133e
wtwerner <wtwerner@gmail.com>
2020-10-23 22:46:12 -0500

    Nest ingredients in recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: "application#welcome"
   resources :steps
-  resources :ingredients
-  resources :recipes
+  resources :recipes do
+    resources :ingredients
+  end
   resources :categories
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
