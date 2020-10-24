de49e899fdf0f33eda9bf4aac64f5f68c27fec97
wtwerner <wtwerner@gmail.com>
2020-10-24 15:00:09 -0500

    Create category form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,23 @@ class CategoriesController < ApplicationController
     def index
         @categories = Category.all
     end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.create(category_params)
+        redirect_to categories_path
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(
+            :id,
+            :user_id,
+            :name
+        )
+    end
 end
