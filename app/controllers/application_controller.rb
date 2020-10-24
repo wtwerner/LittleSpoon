ecbb095239bbf24605d3c1efd43020e854caa0ef
wtwerner <wtwerner@gmail.com>
2020-10-23 21:40:04 -0500

    Create root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    def welcome
+        if user_signed_in?
+            render 'users/show'
+        else
+            render 'welcome'
+        end
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "application#welcome"
   resources :steps
   resources :ingredients
   resources :recipes
