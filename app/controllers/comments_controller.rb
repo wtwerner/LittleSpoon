9977acbf8de00efb854faa42d088772ed1abdd90
wtwerner <wtwerner@gmail.com>
2020-10-24 23:52:55 -0500

    Add commenting on recipes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,46 @@
 class CommentsController < ApplicationController
+    before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+    before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+    def edit
+        
+    end
+
+    def show
+        
+    end
+
+    def create
+        recipe = Recipe.find(params[:recipe_id])
+        @comment = recipe.comments.create(comment_params)
+        redirect_to recipe_path(recipe)
+    end
+
+    def update
+        @comment.update(comment_params)
+        redirect_to @recipe
+    end
+
+    def destroy
+        @comment.destroy
+        redirect_to @recipe
+    end
+
+    private
+
+    def set_recipe
+        @recipe = Recipe.find(params[:recipe_id])
+    end
+
+    def set_comment
+        @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(
+            :content, 
+            :recipe_id,
+            :user_id
+        )
+    end
 end
