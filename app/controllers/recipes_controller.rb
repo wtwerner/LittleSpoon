3d60f20d32fbabc8ffc57b4857b33860980f135f
wtwerner <wtwerner@gmail.com>
2020-10-24 20:47:23 -0500

    Refactor recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,37 +18,17 @@ class RecipesController < ApplicationController
     end
 
     def create
-        @recipe = Recipe.new(recipe_params)
-        
-        respond_to do |format|
-            if @recipe.save
-                format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
-                format.json { render :show, status: :created, location: @recipe }
-            else
-                format.html { render :new }
-                format.json { render json: @recipe.errors, status: :unprocessable_entity }
-            end
-        end
+        @recipe = Recipe.create(recipe_params)
+        redirect_to recipe_path(@recipe)
     end
 
     def update
-        respond_to do |format|
-            if @recipe.update(recipe_params)
-                format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.'}
-                format.json { render :show, status: :ok, location: @recipe }
-            else
-                format.html { render :edit }
-                format.json { render json: @recipe.errors, status: :unprocessable_entity }
-            end
-        end
+        @recipe.update(recipe_params)
     end
 
     def destroy
         @recipe.destroy
-        respond_to do |format|
-            format.html { redirect_to recipes_url, notice: 'Recipe was successfully deleted.' }
-            format.json { head :no_content }
-        end
+        redirect_to recipes_path
     end
 
     private
