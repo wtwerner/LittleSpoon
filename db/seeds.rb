89d9fb47e852953a18a34d934f492b67d0e95323
wtwerner <wtwerner@gmail.com>
2020-10-24 20:57:20 -0500

    Create category seeds

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+Category.destroy_all
+
+Category.create!([
+    {name: "Breakfast"},
+    {name: "Lunch"},
+    {name: "Dinner"},
+    {name: "Dessert"},
+    {name: "Snack"},
+    {name: "Side"},
+    {name: "Drink"},
+    {name: "Chicken"},
+    {name: "Beef"},
+    {name: "Pork"},
+    {name: "Vegetable"},
+    {name: "Soup"},
+    {name: "Bread"},
+    {name: "Salad"},
+    {name: "Stew"},
+    {name: "Fruit"},
+    {name: "Pasta"},
+    {name: "Grilling"},
+    {name: "Seafood"},
+    {name: "Healthy"},
+    {name: "World Cuisine"}
+])
