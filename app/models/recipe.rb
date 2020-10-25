a1e75c4932cd61cb9a001a6be687e2080a8ad6d2
wtwerner <wtwerner@gmail.com>
2020-10-25 13:25:03 -0500

    Refactor delete button and fix association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_categories
+  has_many :recipe_categories, dependent: :destroy
   has_many :categories, through: :recipe_categories
   has_many :steps, dependent: :destroy
   has_many :ingredients, dependent: :destroy
