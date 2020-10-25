477a4042d741604fe99814d1ef134b48ffd38f0a
wtwerner <wtwerner@gmail.com>
2020-10-24 19:56:11 -0500

    Remove ingredient_recipes

--- app/models/ingredient_recipe.rb
+++
@@ -1,4 +0,0 @@
-class IngredientRecipe < ApplicationRecord
-    belongs_to :ingredient, optional: true
-    belongs_to :recipe, optional: true
-end
--- db/migrate/20201024212336_create_ingredient_recipes.rb
+++
@@ -1,8 +0,0 @@
-class CreateIngredientRecipes < ActiveRecord::Migration[6.0]
-  def change
-    create_table :ingredient_recipes do |t|
-      t.belongs_to :ingredient, null: false, foreign_key: true
-      t.belongs_to :recipe, null: false, foreign_key: true
-    end
-  end
-end
