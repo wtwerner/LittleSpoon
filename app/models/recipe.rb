d3da5a57775219d945d6dddc53ec60f61318aee2
wtwerner <wtwerner@gmail.com>
2020-10-24 20:47:34 -0500

    Add destroy dependencies

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,9 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_categories
   has_many :categories, through: :recipe_categories
-  has_many :steps
-  has_many :ingredients
-  has_many :comments
+  has_many :steps, dependent: :destroy
+  has_many :ingredients, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   accepts_nested_attributes_for :ingredients
   accepts_nested_attributes_for :steps
